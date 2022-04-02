bb52a42c316d5a2df960d12fb6afd9b5a6aba2f9
abdoemr11 <abdoemr11@gmail.com>
2022-04-02 16:35:40 +0200

    finished 1.3 and create the Total element that should be done on the previous excercise

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -17,28 +17,42 @@ const Content = (props) =>{
   </>
   );
 }
+const Total =(props) => {
+  return (
+    <p>Number of exercises {props.exer1 + props.exer2 + props.exer3}</p>
+  );
+}
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course = {course}/>
 
-      <Content part1={part1}
-               part2={part2}
-               part3={part3}
-               exercises1 = {exercises1}
-               exercises2 = {exercises2}
-               exercises3 = {exercises3}
+      <Content part1={part1.name}
+               part2={part2.name}
+               part3={part3.name}
+               exercises1 = {part1.exercises}
+               exercises2 = {part2.exercises}
+               exercises3 = {part3.exercises}
 
       />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total
+              exer1 = {part1.exercises}
+              exer2 = {part2.exercises}
+              exer3 = {part3.exercises} />
     </div>
   )
 }
