ded28a4019f94bd25b9a50c4395cc14f82533fb6
abdoemr11 <abdoemr11@gmail.com>
2022-04-16 15:29:59 +0200

    working on 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from 'react'
+const Filter = ({filterName, handleFilterChange})=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    Filter Shown with: <input type="text" value={filterName} onChange={handleFilterChange}/>
+
+    </>
+    );
+  
+}
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      number: '1231-324235235' }
+  ]) 
+  const [newName, setNewName] = useState('');
+  const [newNum, setNewNum] = useState('');
+  const [filterName, setFilterName] = useState('');
+  const [showAll, setShowAll] = useState(true);
+
+
+  const personsToShow = showAll? persons: persons.filter(p=>p.name.toLowerCase().includes(filterName.toLowerCase()));
+  const handleNewNameChange = (event)=>{
+    setNewName(event.target.value);
+  }
+  const handleFilterChange = (e) => {
+    setFilterName(e.target.value);
+    setShowAll(false);
+  }
+  const handleNewNumChange = (e) => {
+    setNewNum(e.target.value);
+  }
+  const addNewPerson = (e) => {
+    e.preventDefault();
+    if(persons.some(p=>p.name === newName)){
+      alert(`${newName} is already added to phonebook`);
+      return;
+
+    }
+    const newPerson = {name : newName, number: newNum};
+    setPersons(persons.concat(newPerson));
+  }
+  return (
+    <div>
+
+      <h2>Phonebook</h2>
+      <Filter filterName={filterName} handleFilterChange={handleFilterChange}/>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewNameChange}/>
+        </div>
+        <div>
+          number: 
+        <input value={newNum} onChange={handleNewNumChange}/>
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {personsToShow.map((p)=><li key={p.name}>{p.name} :  {p.number}</li>)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
