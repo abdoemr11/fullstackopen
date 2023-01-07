217b8135b297b6e73c1fe4dc6b04d330ad2780e7
abdoemr11 <abdoemr11@gmail.com>
2023-01-07 22:32:39 +0200

    part 7 finish user edit

--- part7/bloglist/frontend/src/routes/Users.js
+++ part7/bloglist/frontend/src/routes/Users.js
@@ -1,29 +1,32 @@
 import {  useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import Avatar from 'boring-avatars'
 // import Avatar from 'boring-avatars'
 export const Users = () => {
   const users = useSelector(state => state.users)
 
 
   return(
-    <div>
+    <div className='container border-2 border-mainBlue mx-4 p-8'  >
       <div style={{ display: 'flex',
         width: '30%',
         justifyContent: 'center'
       }}>
-        Blog Created
       </div>
       {users.map(u => (
         <div
-          style = {{ display: 'flex',
-            width: '30%'
-          }}
+          className=' flex border border-mainBlue m-4 p-4'
           key={u.id}>
 
-          <Link
+          <Link className='flex w-1/3 gap-2'
             to={u.id}
-            style = {{ flex: '1' }}
-          >{u.name}</Link>
+
+          >
+            <Avatar name={u.name}
+              variant="beam"
+            />
+            {u.name}
+          </Link>
           <span style={{ flex: '1' }}>{u.blogs.length}</span>
         </div>
       ))}
